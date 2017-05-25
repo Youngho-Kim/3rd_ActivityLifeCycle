3e5e9ec3d052447693f8a81fe605bf1d9db5b19b
javafa <nigne2016@gmail.com>
2017-05-25 16:30:26 +0900

    init

--- app/src/main/java/com/veryworks/android/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/veryworks/android/activitylifecycle/MainActivity.java
@@ -1,8 +1,10 @@
 package com.veryworks.android.activitylifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -12,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "============ onCreate");
         setContentView(R.layout.activity_main);
+        // 1. 액티비티를 두개 생성한다
+        //    가.GeneralActivity , 나.TransparentActivity
+        //
+        // 2. 현재 액티비티에 버튼두개를 생성한 후에
+        //    위에서 만든 각 Activity를 호출하세요
+        // Normal
+        findViewById(R.id.btnNormal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Translucent
+        findViewById(R.id.btnTranslucent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TransActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/veryworks/android/activitylifecycle/TransActivity.java
@@ -0,0 +1,14 @@
+package com.veryworks.android.activitylifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TransActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trans);
+
+    }
+}
